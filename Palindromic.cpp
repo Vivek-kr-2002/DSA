fca19e59e99b1d3f7a2d8ebbbebe576d19bf1bef
Vivek Kumar <103559011+Vivek-kr-2002@users.noreply.github.com>
2023-03-13 00:22:04 +0530

    Create Palindromic.cpp

---
+++ Palindromic.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define nn                    '\n' 
+#define pb                    push_back 
+#define SS                    stringstream 
+#define ull                   unsigned long long 
+#define mod                   1000000007 
+#define INF                   2147483647 
+#define SIZE                  2000001  
+#define sf1(a)                scanf("%d", &a) 
+#define sf2(a,b)              scanf("%d %d",&a, &b) 
+#define sf3(a,b,c)            scanf("%d %d %d", &a, &b, &c) 
+#define sf4(a,b,c, d)         scanf("%d %d %d %d", &a, &b, &c, &d)
+ #define sf(n) scanf("%d", &n)
+#define sff(n,m) scanf("%d%d",&n,&m)
+#define sfl(n) scanf("%I64d", &n)
+#define sffl(n,m) scanf("%I64d%I64d",&n,&m)
+#define pf(n) printf("%d",n)
+#define pff(n,m) printf("%d %d",n,m)
+#define pffl(n,m) printf("%I64d %I64d",n,m)
+#define pfl(n) printf("%I64d",n)
+#define pfs(s) printf("%s",s)
+#define lli long long int
+#define pb push_back
+#define f first
+#define sec second
+
+
+int main()
+{
+	string str;
+	cout<<"Enter a string: ";
+	getline(cin,str);
+	int len=str.length();
+	int i=len-1;
+	int flag=0;
+	for (int j = 0; i<j; j++,i--)
+	{
+		if (str[i] != str[j])
+		{
+			flag=1;
+			cout<<"\n Entered string is not a Palindrome.\n";
+			break;
+		}
+	}
+	if (flag==0)
+		cout<<"\n Entered string is a Palindrome.\n";
+	return 0;
+}
